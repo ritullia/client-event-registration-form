660bb65942b685a8771bc63fcb6b0882610284ea
ritullia <brigrita@gmail.com>
2023-05-18 16:42:30 +0300

    login form commit

--- front-end/events-registration-app/src/components/Login.jsx
+++ front-end/events-registration-app/src/components/Login.jsx
@@ -1,9 +1,45 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 export const Login = () => {
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Papsaustas prisijungti mygtukas`);
+  };
   return (
     <>
-      <div>
-        <p>LOGIN FORM</p>
-      </div>
+      <>
+        <Form
+          onSubmit={onHandleSubmit}
+          style={{
+            maxWidth: "340px",
+            margin: "50px auto",
+            backgroundColor: "#4b5162",
+            padding: "20px",
+            color: "#ffff",
+            borderRadius: "8px",
+          }}
+        >
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" required />
+          </Form.Group>
+          <Button
+            //   onClick={() => alert("Įvestas naujas klientas")}
+            variant="primary"
+            type="submit"
+          >
+            Log in
+          </Button>
+        </Form>
+      </>
     </>
   );
 };
