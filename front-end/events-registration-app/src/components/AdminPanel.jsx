02055f17b8f6458e28df3b51f6ad0c8f4677c608
ritullia <brigrita@gmail.com>
2023-05-19 16:58:01 +0300

    admin panel form commit

--- front-end/events-registration-app/src/components/AdminPanel.jsx
+++ front-end/events-registration-app/src/components/AdminPanel.jsx
@@ -1,10 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
 
 export const AdminPanel = () => {
+  const [data, setData] = useState({
+    name: "",
+    lastname: "",
+    phone_number: "",
+    email: "",
+  });
+
+  const navigate = useNavigate();
+
+  console.log("Submitinau", data);
+
   const onHandleSubmit = (e) => {
     e.preventDefault();
     alert(`Ivestas naujas dalyvis`);
+
+    axios.post("http://localhost:5000/clients", data).then((response) => {
+      console.log(response.data);
+      navigate("/events");
+    });
+  };
+
+  const handleOnChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
   };
   return (
     <>
@@ -21,23 +47,43 @@ export const AdminPanel = () => {
       >
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Client name" required />
+          <Form.Control
+            name="name"
+            type="text"
+            placeholder="Enter Client name"
+            onChange={handleOnChange}
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Lastname</Form.Label>
           <Form.Control
             type="text"
+            name="lastname"
             placeholder="Enter Client lastname"
+            onChange={handleOnChange}
             required
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Phone number</Form.Label>
-          <Form.Control type="text" placeholder="Enter phone number" required />
+          <Form.Control
+            name="phone_number"
+            type="text"
+            placeholder="Enter phone number"
+            onChange={handleOnChange}
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required />
+          <Form.Control
+            name="email"
+            type="email"
+            placeholder="Enter email"
+            onChange={handleOnChange}
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
